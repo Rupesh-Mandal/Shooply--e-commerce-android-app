6865889c65f76b01372c887f657a527d1a05c9a0
rupesh <mandalrupesh883@gmail.com>
2022-03-09 17:27:16 +0545

    4

--- app/src/main/java/com/firoz/shooply/checkout/adapter/PaymentListAdapter.java
+++ app/src/main/java/com/firoz/shooply/checkout/adapter/PaymentListAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firoz.shooply.R;
 import com.firoz.shooply.model.CartModel;
 
@@ -22,56 +24,69 @@ import java.util.List;
 public class PaymentListAdapter extends RecyclerView.Adapter<PaymentListAdapter.myViewHolder> {
 
     Context context;
-    JSONArray orderArray;
+    List<CartModel> selectCartModelList;
 
-
-    public PaymentListAdapter(Context context, JSONArray orderArray) {
+    public PaymentListAdapter(Context context, List<CartModel> selectCartModelList) {
         this.context = context;
-        this.orderArray = orderArray;
+        this.selectCartModelList = selectCartModelList;
     }
 
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.payment_list_item,null,false);
+        View view= LayoutInflater.from(context).inflate(R.layout.checkout_item,null,false);
         return new myViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
-        try {
-            JSONObject jsonObject=orderArray.getJSONObject(position);
-            holder.name.setText(jsonObject.getString("productName"));
-            holder.quantity.setText(jsonObject.getString("quantity"));
-            holder.rate.setText(jsonObject.getString("productRate"));
+        CartModel cartModel=selectCartModelList.get(position);
+        Glide.with(context).load(cartModel.getProductImageLink()).into(holder.productImageLink);
 
-            double q= Double.parseDouble(jsonObject.getString("quantity"));
-            double r= Double.parseDouble(jsonObject.getString("productRate"));
-            double a=q*r;
+        double r1= Double.parseDouble(cartModel.getProductRate());
+        double m1= Double.parseDouble(cartModel.getMrp());
+        double c1 = Double.parseDouble(cartModel.getQuantity());
 
-            holder.amount.setText(String.valueOf(a));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        double total=r1*c1;
 
+        double totalWithOutDiscount=r1*c1;
+        double discountAmount=totalWithOutDiscount-total;
+
+        double d = Double.parseDouble(cartModel.getDiscount());
+        int discount = (int) d;
+
+
+        holder.productName.setText(cartModel.getProductName());
+       holder.productDescription.setText(cartModel.getProductDescription());
+       holder.productRate.setText(cartModel.getProductRate());
+       holder.mrp.setText(cartModel.getMrp());
+       holder.discount.setText("-"+discount+"%");
+       holder.productQuantity.setText(cartModel.getQuantity());
+       holder.totalRate.setText(String.valueOf(total));
+       holder.discountAmount.setText(String.valueOf(discountAmount));
 
     }
 
     @Override
     public int getItemCount() {
-        return orderArray.length();
+        return selectCartModelList.size();
     }
 
 
     class myViewHolder extends RecyclerView.ViewHolder {
-        TextView name,quantity,rate,amount;
-
+        TextView productName,productDescription,productRate,mrp,discount,productQuantity,totalRate,discountAmount;
+        ImageView productImageLink;
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
-            name=itemView.findViewById(R.id.name);
-            quantity=itemView.findViewById(R.id.quantity);
-            rate=itemView.findViewById(R.id.rate);
-            amount=itemView.findViewById(R.id.amount);
+            productName=itemView.findViewById(R.id.productName);
+            productDescription=itemView.findViewById(R.id.productDescription);
+            productRate=itemView.findViewById(R.id.productRate);
+            mrp=itemView.findViewById(R.id.mrp);
+            discount=itemView.findViewById(R.id.discount);
+            productQuantity=itemView.findViewById(R.id.productQuantity);
+            totalRate=itemView.findViewById(R.id.totalRate);
+            discountAmount=itemView.findViewById(R.id.discountAmount);
+            productImageLink=itemView.findViewById(R.id.productImageLink);
         }
     }
 }
--- app/src/main/java/com/firoz/shooply/user_dashboard/activity/CheckOutActivity.java
+++ app/src/main/java/com/firoz/shooply/user_dashboard/activity/CheckOutActivity.java
@@ -3,6 +3,7 @@ package com.firoz.shooply.user_dashboard.activity;
 import static com.firoz.shooply.util.Constant.oderProduct;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +15,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,8 @@ import com.firoz.shooply.model.CartModel;
 import com.firoz.shooply.user_dashboard.UserDashboardActivity;
 import com.firoz.shooply.user_dashboard.helper.CartHelper;
 import com.firoz.shooply.util.ResponsListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,32 +46,68 @@ import java.util.List;
 
 public class CheckOutActivity extends AppCompatActivity {
 
+    ConstraintLayout addressLayout;
     RecyclerView payment_recyclerView;
     Button cash_on_delivery;
     ProgressDialog progressDialog;
-    JSONArray orderArray = new JSONArray();
+    List<CartModel> selectCartModelList = new ArrayList<>();
 
+    TextView productDeliverAddress,userPhoneNumber,addAddress,total,totalMrp;
+    ImageView editAddress;
     CartHelper cartHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
-        payment_recyclerView=findViewById(R.id.payment_recyclerView);
-        cash_on_delivery=findViewById(R.id.cash_on_delivery);
-        progressDialog=new ProgressDialog(this);
-        progressDialog.setMessage("Please Waite");
-        String object=getIntent().getStringExtra("orderArray");
-        try {
-            orderArray=new JSONArray(object);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        initView();
+
+        String object = getIntent().getStringExtra("selectCartModelList");
+        selectCartModelList = new Gson().fromJson(object, new TypeToken<List<CartModel>>() {
+        }.getType());
+
         cartHelper=new CartHelper(this);
 
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Please Waite");
+
         payment_recyclerView.setLayoutManager(new GridLayoutManager(this,1));
-        PaymentListAdapter paymentListAdapter=new PaymentListAdapter(this,orderArray);
+        PaymentListAdapter paymentListAdapter=new PaymentListAdapter(this,selectCartModelList);
         payment_recyclerView.setAdapter(paymentListAdapter);
 
+
+    }
+
+    private void initView() {
+        addressLayout=findViewById(R.id.addressLayout);
+        payment_recyclerView=findViewById(R.id.payment_recyclerView);
+        cash_on_delivery=findViewById(R.id.cash_on_delivery);
+        productDeliverAddress=findViewById(R.id.productDeliverAddress);
+        userPhoneNumber=findViewById(R.id.userPhoneNumber);
+        addAddress=findViewById(R.id.addAddress);
+        total=findViewById(R.id.total);
+        totalMrp=findViewById(R.id.totalMrp);
+        editAddress=findViewById(R.id.editAddress);
+
+
+        double r=0;
+        double m=0;
+        double totalPr=0;
+        double totalMr=0;
+
+        for (int i=0;i<selectCartModelList.size();i++){
+            double rate= Double.parseDouble(selectCartModelList.get(i).getProductRate());
+            double mrp= Double.parseDouble(selectCartModelList.get(i).getMrp());
+            double quentity= Double.parseDouble(selectCartModelList.get(i).getQuantity());
+
+            r=r+rate;
+            m=m+mrp;
+
+            totalPr=totalPr+(rate*quentity);
+            totalMr=totalMr+(mrp*quentity);
+        }
+
+        total.setText(String.valueOf(totalPr));
+        totalMrp.setText(String.valueOf(totalMr));
 
         cash_on_delivery.setOnClickListener(view -> {
             progressDialog.show();
